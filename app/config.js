81e0497066c88c85d206fcd816a740817424b284
Emma McBride <emma.mcbride@digital.homeoffice.gov.uk>
2019-10-28 11:41:38 +0000

    Version 8: Stage 2 and 5 V1

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Identity and authentication',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,78 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// Biographic match //
+
+router.post('/biographic-match-answer', function (req, res) {
+
+  var biographicMatch = req.session.data['biographic-match']
+
+
+// Check whether the variable matches a condition
+  if (biographicMatch == "yes"){
+    // Send user to next page
+    res.redirect('/version8/stage2/document-check')
+  }
+  if (biographicMatch == "no"){
+    // Send user to next page
+    res.redirect('/version8/stage2/document-check')
+  }
+  if (biographicMatch == 'theres-a-small-difference'){
+    // Send user to next page
+    res.redirect('#')
+  }
+
+
+})
+
+// Document match //
+
+router.post('/document-match-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var biographicMatch = req.session.data['document-match']
+
+
+// Check whether the variable matches a condition
+  if (biographicMatch == "yes"){
+    // Send user to next page
+    res.redirect('/version8/stage2/check-photo')
+  }
+  if (biographicMatch == "no"){
+    // Send user to next page
+    res.redirect('/version8/stage2/check-photo')
+  }
+  if (biographicMatch == 'theres-a-small-difference'){
+    // Send user to next page
+    res.redirect('#')
+  }
+
+
+})
+
+// Request decision //
+
+router.post('/request-decision-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var requestDecision = req.session.data['request-decision']
+
+
+// Check whether the variable matches a condition
+  if (requestDecision == "yes"){
+    // Send user to next page
+    res.redirect('/version8/stage2/submission-updated')
+  }
+  if (requestDecision == "no"){
+    // Send user to next page
+    res.redirect('/version8/stage2/submission-rejected')
+  }
+  if (requestDecision == "refer"){
+    // Send user to next page
+    res.redirect('/version8/stage2/submission-referred')
+  }
+
+
+})
 
 module.exports = router
