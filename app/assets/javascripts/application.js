cbad87d39d1994fdbc6ca3083f9ba829e1c10561
Emma McBride <emma.mcbride@digital.homeoffice.gov.uk>
2019-11-12 15:11:10 +0000

    Exprimental pages added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,61 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+
+// Select all checkboxes
+
+$(".selectall.govuk-checkboxes__input").click(function(){
+$(".individual.govuk-checkboxes__input").prop("checked",$(this).prop("checked"));
+});
+
+// Filter and sort //
+//
+// {% from "govuk/components/select/macro.njk" import govukSelect %}
+//
+// {{ govukSelect({
+//   id: "sort",
+//   name: "sort",
+//   label: {
+//     text: "Sort by"
+//   },
+//   items: [
+//     {
+//       value: "all",
+//       text: "Show all",
+//       selected: true
+//     },
+//     {
+//       value: "none",
+//       text: "None",
+//     },
+//     {
+//       value: "a",
+//       text: "Hold A"
+//     },
+//     {
+//       value: "b",
+//       text: "Hold B"
+//     }
+//     {
+//       value: "c",
+//       text: "Hold C"
+//     }
+//     {
+//       value: "d",
+//       text: "Hold D"
+//     }
+//     {
+//       value: "e",
+//       text: "Hold E"
+//     }
+//     {
+//       value: "f",
+//       text: "Hold F"
+//     }
+//     {
+//       value: "g",
+//       text: "Hold G"
+//     }
+//   ]
+// }) }}
--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-// Biographic match //
+
+
+// Biographic match V8 S2//
 
 router.post('/biographic-match-answer', function (req, res) {
 
@@ -26,24 +28,24 @@ router.post('/biographic-match-answer', function (req, res) {
 
 })
 
-// Document match //
+// Document match V8 S2//
 
 router.post('/document-match-answer', function (req, res) {
 
 // Make a variable and give it the value from 'how-many-balls'
-  var biographicMatch = req.session.data['document-match']
+  var documentMatch = req.session.data['document-match']
 
 
 // Check whether the variable matches a condition
-  if (biographicMatch == "yes"){
+  if (documentMatch == "yes"){
     // Send user to next page
     res.redirect('/version8/stage2/check-photo')
   }
-  if (biographicMatch == "no"){
+  if (documentMatch == "no"){
     // Send user to next page
     res.redirect('/version8/stage2/check-photo')
   }
-  if (biographicMatch == 'theres-a-small-difference'){
+  if (documentMatch == 'theres-a-small-difference'){
     // Send user to next page
     res.redirect('/version8/stage2/document-check-edit')
   }
@@ -51,7 +53,33 @@ router.post('/document-match-answer', function (req, res) {
 
 })
 
-// Request decision //
+// Photo match V8 S2//
+
+router.post('/photo-check-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var photoCheck = req.session.data['photo-check']
+
+
+// Check whether the variable matches a condition
+  if (photoCheck == "yes"){
+    // Send user to next page
+    res.redirect('/version8/stage2/review-and-submit')
+  }
+  if (photoCheck == "no"){
+    // Send user to next page
+    res.redirect('/version8/stage2/review-and-submit')
+  }
+  // if (biographicMatch == 'theres-a-small-difference'){
+  //   // Send user to next page
+  //   res.redirect('/version8/stage2/document-check-edit')
+  // }
+
+
+})
+
+
+// Request decision V8 S2//
 
 router.post('/request-decision-answer', function (req, res) {
 
@@ -76,7 +104,7 @@ router.post('/request-decision-answer', function (req, res) {
 
 })
 
-// Request decision //
+// Request decision V8 S2//
 
 router.post('/biographic-edit-answer', function (req, res) {
 
@@ -93,7 +121,7 @@ router.post('/biographic-edit-answer', function (req, res) {
 
 })
 
-// Request decision //
+// Request decision V8 S2//
 
 router.post('/document-edit-answer', function (req, res) {
 
@@ -110,7 +138,7 @@ router.post('/document-edit-answer', function (req, res) {
 
 })
 
-// Request decision //
+// Photo check V8 S2//
 
 router.post('/photo-check-answer', function (req, res) {
 
@@ -130,7 +158,7 @@ router.post('/photo-check-answer', function (req, res) {
 
 })
 
-// Biographic match advanced //
+// Biographic match advanced V8 S5//
 
 router.post('/biographic-match-adv-answer', function (req, res) {
 
@@ -154,10 +182,487 @@ router.post('/biographic-match-adv-answer', function (req, res) {
 
 })
 
-// Select all check boxes //
+// Document match advanced V8 S5//
 
-$(".selectall").click(function(){
-$(".individual").prop("checked",$(this).prop("checked"));
-});
+router.post('/document-match-adv-answer', function (req, res) {
+
+  var documentMatchAdv = req.session.data['document-match-adv']
+
+
+// Check whether the variable matches a condition
+  if (documentMatchAdv == "yes"){
+    // Send user to next page
+    res.redirect('/version8/stage5/check-photo')
+  }
+  if (documentMatchAdv == "no"){
+    // Send user to next page
+    res.redirect('/version8/stage5/check-photo')
+  }
+  if (documentMatchAdv == 'theres-a-small-difference'){
+    // Send user to next page
+    res.redirect('/version8/stage5/check-photo')
+  }
+
+
+})
+
+
+// Request decision V8 S5//
+
+router.post('/photo-check-adv-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var photoCheckAdv = req.session.data['photo-check-adv']
+
+
+// Check whether the variable matches a condition
+  if (photoCheckAdv == "yes"){
+    // Send user to next page
+    res.redirect('/version8/stage5/review-and-submit')
+  }
+  if (photoCheckAdv == "no"){
+    // Send user to next page
+    res.redirect('/version8/stage5/review-and-submit')
+  }
+
+})
+
+// Request Decision V8 S5 //
+
+
+router.post('/request-decision-adv-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var requestDecisionAdv = req.session.data['request-decision-adv']
+
+
+// Check whether the variable matches a condition
+  if (requestDecisionAdv == "yes"){
+    // Send user to next page
+    res.redirect('/version8/stage5/application-updated')
+  }
+  if (requestDecisionAdv == "no"){
+    // Send user to next page
+    res.redirect('/version8/stage5/notify-rejection')
+  }
+  if (requestDecisionAdv == "refer"){
+    // Send user to next page
+    res.redirect('/version8/stage5/application-referred')
+  }
+
+
+})
+
+// Request Decision V8 Superuser //
+
+
+router.post('/request-decision-su-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var requestDecisionSu = req.session.data['request-decision-su']
+
+
+// Check whether the variable matches a condition
+  if (requestDecisionSu == "yes"){
+    // Send user to next page
+    res.redirect('/version8/superuser/task-list-updated')
+  }
+  if (requestDecisionSu == "no"){
+    // Send user to next page
+    res.redirect('/version8/superuser/task-list-updated')
+  }
+  if (requestDecisionSu == "hold"){
+    // Send user to next page
+    res.redirect('/version8/superuser/notify-rejection')
+  }
+  if (requestDecisionSu == "void"){
+    // Send user to next page
+    res.redirect('/version8/superuser/task-list-updated')
+  }
+
+
+})
+
+
+// Filter pages EXPERIMENTAL//
+
+router.post('/sort-hold-type', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var holdType = req.session.data['sort-hold-type']
+
+
+// Check whether the variable matches a condition
+  if (holdType == "all"){
+    // Send user to next page
+    res.redirect('/version9/experimental/filter-option')
+  }
+  if (holdType == "none"){
+    // Send user to next page
+    res.redirect('/version9/experimental/filter-option-none')
+  }
+  if (holdType == "a"){
+    // Send user to next page
+    res.redirect('/version9/experimental/filter-option-a')
+  }
+  if (holdType == "b"){
+    // Send user to next page
+    res.redirect('/version9/experimental/filter-option-b')
+  }
+  if (holdType == "c"){
+    // Send user to next page
+    res.redirect('/version9/experimental/filter-option-c')
+  }
+})
+
+// --------------------------------------------------------------------------- //
+
+// Biographic match V9 S2//
+
+router.post('/92-biographic-match-answer', function (req, res) {
+
+  var biographicMatchB = req.session.data['92-biographic-match']
+
+
+// Check whether the variable matches a condition
+  if (biographicMatchB == "yes"){
+    // Send user to next page
+    res.redirect('/version9/stage2/document-check')
+  }
+  if (biographicMatchB == "no"){
+    // Send user to next page
+    res.redirect('/version9/stage2/document-check')
+  }
+  if (biographicMatchB == 'theres-a-small-difference'){
+    // Send user to next page
+    res.redirect('/version9/stage2/biographics-check-edit')
+  }
+
+
+})
+
+// Document match V9 S2//
+
+router.post('/92-document-match-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var documentMatchB = req.session.data['92-document-match']
+
+
+// Check whether the variable matches a condition
+  if (documentMatchB == "yes"){
+    // Send user to next page
+    res.redirect('/version9/stage2/check-photo')
+  }
+  if (documentMatchB == "no"){
+    // Send user to next page
+    res.redirect('/version9/stage2/check-photo')
+  }
+  if (documentMatchB == 'theres-a-small-difference'){
+    // Send user to next page
+    res.redirect('/version9/stage2/document-check-edit')
+  }
+
+
+})
+
+// Photo match V9 S2//
+
+router.post('/92-photo-check-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var photoCheckB = req.session.data['92-photo-check']
+
+
+// Check whether the variable matches a condition
+  if (photoCheckB == "yes"){
+    // Send user to next page
+    res.redirect('/version9/stage2/review-and-submit')
+  }
+  if (photoCheckB == "no"){
+    // Send user to next page
+    res.redirect('/version9/stage2/review-and-submit')
+  }
+
+
+})
+
+// Request decision V9 S2//
+
+router.post('/92-request-decision-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var requestDecisionB = req.session.data['92-request-decision']
+
+
+// Check whether the variable matches a condition
+  if (requestDecisionB == "yes"){
+    // Send user to next page
+    res.redirect('/version9/stage2/submission-updated')
+  }
+  if (requestDecisionB == "no"){
+    // Send user to next page
+    res.redirect('/version9/stage2/notify-rejection')
+  }
+  if (requestDecisionB == "refer"){
+    // Send user to next page
+    res.redirect('/version9/stage2/submission-referred')
+  }
+
+
+})
+
+// Biographic match V9 S5//
+
+router.post('/95-biographic-match-answer', function (req, res) {
+
+  var biographicMatchC = req.session.data['95-biographic-match']
+
+
+// Check whether the variable matches a condition
+  if (biographicMatchC == "yes"){
+    // Send user to next page
+    res.redirect('/version9/stage5/document-check')
+  }
+  if (biographicMatchC == "no"){
+    // Send user to next page
+    res.redirect('/version9/stage5/document-check')
+  }
+  if (biographicMatchC == 'theres-a-small-difference'){
+    // Send user to next page
+    res.redirect('/version9/stage5/document-check')
+  }
+
+
+})
+
+// Document match V9 S5//
+
+router.post('/95-document-match-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var documentMatchC = req.session.data['95-document-match']
+
+
+// Check whether the variable matches a condition
+  if (documentMatchC == "yes"){
+    // Send user to next page
+    res.redirect('/version9/stage5/check-photo')
+  }
+  if (documentMatchC == "no"){
+    // Send user to next page
+    res.redirect('/version9/stage5/check-photo')
+  }
+  if (documentMatchC == 'theres-a-small-difference'){
+    // Send user to next page
+    res.redirect('/version9/stage5/check-photo')
+  }
+
+
+})
+
+// Photo match V9 S5//
+
+router.post('/95-photo-check-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var photoCheckC = req.session.data['95-photo-check']
+
+
+// Check whether the variable matches a condition
+  if (photoCheckC == "yes"){
+    // Send user to next page
+    res.redirect('/version9/stage5/review-and-submit')
+  }
+  if (photoCheckC == "no"){
+    // Send user to next page
+    res.redirect('/version9/stage5/review-and-submit')
+  }
+
+
+})
+
+// Request decision V9 S5//
+
+router.post('/95-request-decision-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var requestDecisionC = req.session.data['95-request-decision']
+
+
+// Check whether the variable matches a condition
+  if (requestDecisionC == "yes"){
+    // Send user to next page
+    res.redirect('/version9/stage5/application-updated')
+  }
+  if (requestDecisionC == "no"){
+    // Send user to next page
+    res.redirect('/version9/stage5/notify-rejection')
+  }
+  if (requestDecisionC == "refer"){
+    // Send user to next page
+    res.redirect('/version9/stage5/application-referred')
+  }
+
+
+})
+
+// Request decision V9 SU//
+
+router.post('/request-decision-9s-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var requestDecisionD = req.session.data['request-decision-9s']
+
+
+// Check whether the variable matches a condition
+  if (requestDecisionD == "yes"){
+    // Send user to next page
+    res.redirect('/version9/superuser/task-list-updated')
+  }
+  if (requestDecisionD == "no"){
+    // Send user to next page
+    res.redirect('/version9/superuser/notify-rejection')
+  }
+  if (requestDecisionD == "hold"){
+    // Send user to next page
+    res.redirect('/version9/superuser/task-list-updated')
+  }
+  if (requestDecisionD == "withdraw"){
+    // Send user to next page
+    res.redirect('/version9/superuser/task-list-updated')
+  }
+
+
+})
+
+//--------------------------------------------------------
+
+
+// Biographic match V9 CC//
+
+router.post('/9cc-biographic-match-answer', function (req, res) {
+
+  var biographicMatchD = req.session.data['9cc-biographic-match']
+
+
+// Check whether the variable matches a condition
+  if (biographicMatchD == "yes"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/document-check')
+  }
+  if (biographicMatchD == "no"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/document-check')
+  }
+  if (biographicMatchD == 'theres-a-small-difference'){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/biographics-check-edit')
+  }
+
+
+})
+
+// Document match V9 CC//
+
+router.post('/9cc-document-match-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var documentMatchD = req.session.data['9cc-document-match']
+
+
+// Check whether the variable matches a condition
+  if (documentMatchD == "yes"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/facial-likeness')
+  }
+  if (documentMatchD == "no"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/facial-likeness')
+  }
+
+
+})
+
+// Photo match V9 CC//
+
+router.post('/9cc-photo-match-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var photoCheckD = req.session.data['9cc-photo-match']
+
+
+// Check whether the variable matches a condition
+  if (photoCheckD == "yes"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/review-and-submit')
+  }
+  if (photoCheckD == "no"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/review-and-submit')
+  }
+
+
+})
+
+// Request decision V9 CC//
+
+router.post('/9cc-request-decision-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var requestDecisionD = req.session.data['9cc-request-decision']
+
+
+// Check whether the variable matches a condition
+  if (requestDecisionD == "yes"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/application-updated')
+  }
+  if (requestDecisionD == "no"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/notify-rejection')
+  }
+  if (requestDecisionD == "contact"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/contact-customer')
+  }
+  if (requestDecisionD == "refer"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/application-referred')
+  }
+
+
+})
+
+// Request decision V9 CC//
+
+router.post('/hold-decision-answer', function (req, res) {
+
+// Make a variable and give it the value from 'how-many-balls'
+  var holdDecision = req.session.data['hold-decision']
+
+
+// Check whether the variable matches a condition
+  if (holdDecision == "open"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/application-updated')
+  }
+  if (holdDecision == "later-time"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/application-updated')
+  }
+  if (holdDecision == "spec-time"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/application-updated')
+  }
+  if (holdDecision == "spec-date"){
+    // Send user to next page
+    res.redirect('/version9/chipcheck/application-updated')
+  }
+
+
+})
+//
+// Do not delete below //
 
 module.exports = router
